c2699022d1754c7dbd0c2090d6d702bd2d126ed4
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-03-04 21:30:23 +0530

    add FeedBack component with initial feedback message

---
+++ first_app/src/components/FeedBack.jsx
@@ -0,0 +1,12 @@
+function FeedBack()
+{
+    return(
+    <>
+    
+    <h1 style={{fontFamily: "monospace", textAlign: "center"}}>I am a Feedback Page</h1>
+    <h1 style={{fontFamily: "monospace", textAlign: "center"}}>Your Feedback is valuable for us 😊</h1>
+    </>
+    )
+}
+
+export default FeedBack
