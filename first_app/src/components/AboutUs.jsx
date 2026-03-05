742c32acabcb355155ba98c4f1d10ced14b1d7ec
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-03-05 20:11:04 +0530

    add AboutUs component with initial content and ContactUs integration

---
+++ first_app/src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+import ContactUs from "./ContactUs"
+function AboutUs()
+{
+    return (
+     <>
+      <p>I am about us page</p>
+      <p>I am about us page</p>
+      <p>I am about us page</p>
+      <p>I am about us page</p>
+      <ContactUs/>
+    </>
+)};
+
+
+export default AboutUs
