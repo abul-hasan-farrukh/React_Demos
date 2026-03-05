0e50b7a1ec0b12121db57492cdffcc56ed8266a1
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-03-05 20:10:26 +0530

    add App component with home page structure and AboutUs integration

---
+++ first_app/src/App.jsx
@@ -0,0 +1,17 @@
+import AboutUs from './components/AboutUs.jsx'
+import './css/app.css' 
+
+function App() {
+  
+
+  return (
+    <> 
+    <div className='bg'>
+      <h1>This is Home Page</h1>
+    </div>
+      <AboutUs/>
+    </>
+  )
+}
+
+  export default App
