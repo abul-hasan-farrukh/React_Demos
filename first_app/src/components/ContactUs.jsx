6383437c1585f25d048beca3900f5470e1f19874
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-03-05 20:14:00 +0530

    add ContactUs component with form fields and FeedBack integration

---
+++ first_app/src/components/ContactUs.jsx
@@ -0,0 +1,22 @@
+import FeedBack from "./FeedBack"
+function ContactUs()
+{
+    return (
+        <>
+        <div>
+         <h2>I am Contact Us Page</h2>
+        <form action="">
+            <label htmlFor="">Name:</label>
+            <input type="text" name="" id="" />
+            <label htmlFor="">Email Id:</label>
+            <input type="text" name="" id="" />
+        </form>
+         <FeedBack/>
+        </div>
+        
+        </>
+    ) 
+
+}
+
+export default ContactUs
