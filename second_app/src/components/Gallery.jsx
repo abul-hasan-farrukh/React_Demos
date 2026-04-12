6b2941e56376aeca844c909addb9f3e6f1fb9034
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-04-12 17:32:32 +0530

    add Gallery component

---
+++ second_app/src/components/Gallery.jsx
@@ -0,0 +1,21 @@
+function Gallery()
+{
+    return(
+        <>
+        <div className="flexContainer">
+            <div className="boxItem">
+                <img className="img" src="/images/egg1-removebg-preview.png" alt=""/>
+            </div>
+            <div className="boxItem">
+                <img className="img" src="/images/egg2-removebg-preview.png" alt=""/>
+
+            </div>
+            <div className="boxItem">
+                <img className="chuza" src="/images/chuza-removebg-preview.png" alt=""/>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Gallery
