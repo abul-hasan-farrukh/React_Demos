bbc23619ad421d0f0454c1260bd4ffcd812bcd3d
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-04-13 14:47:42 +0530

    add Header component

---
+++ second_app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header()
+{
+    return(
+        <>
+        <div style={{margin: "0px", textAlign:"center", width: "100%", height: "50px", backgroundColor: "aqua"}}>
+        <i className="fas fa-home" style={{fontSize:"0px"}}></i>
+            <h2 style={{paddingTop:"10px", fontFamily: "Monotype Corsiva"}}>Header Text</h2>
+        </div>
+        </>
+    )
+}
+
+export default Header
