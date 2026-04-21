16f8a16690991227a7dd014c62073452a62476a3
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-04-21 12:30:15 +0530

    add App component

---
+++ third_app/src/App.jsx
@@ -0,0 +1,19 @@
+import Accordion from './components/Accordion'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import RegistrationForm from './components/RegistrationForm'
+import './css/registration_form.css'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Accordion/>
+      <RegistrationForm/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
