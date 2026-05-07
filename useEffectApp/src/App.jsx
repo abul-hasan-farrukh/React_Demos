c7f47757b89ebea0e305b6fc02eb8a0c14656124
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-05-07 09:39:26 +0530

    add App.jsx component

---
+++ useEffectApp/src/App.jsx
@@ -0,0 +1,17 @@
+import Notice from "./components/Notice"
+import Product from "./components/Product"
+import './css/cardStyle.css'
+import './css/tableStyle.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Notice/> 
+      <Product/>   
+    </>
+  )
+}
+
+export default App
